f03c69292da3161b0c155731c9ede9f1edda9186
Nico Hamaus <nhamaus@gmail.com>
2024-02-12 15:51:36 +0100

    Ignore _static folder in docs and disable pdf output

--- docs/conf.py
+++ docs/conf.py
@@ -43,10 +43,10 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
+html_static_path = []
 html_logo = 'voiager.png'
